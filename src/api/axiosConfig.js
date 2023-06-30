64e5049641655d6734745a8c1872a183d1791f4d
Dss <sd;fjkwoiefjsldkfj@adjskjd.org>
2023-06-30 14:59:48 +0200

    Layout.js Home.js Hero.js Hero.css Trailer.js ReviewForm.js Reviews.js NotFound.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,6 +4,5 @@ export default axios.create({
   // baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
   // headers: {"ngrok-skip-browser-warning": "true"}
 
-  baseURL: 'https://127.0.0.1',
-  headers: {"127.0.0.1-skip-browser-warning": "true"}
+  baseURL: 'http://127.0.0.1:8080',
 });
