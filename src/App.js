99425c9fd2ccee452042dbb6bd8d9041636cfd88
Dss <sd;fjkwoiefjsldkfj@adjskjd.org>
2023-07-04 18:02:23 +0200

    issue with reviews closed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
-import NotFound from './components/notfound/NotFound';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
@@ -16,10 +16,14 @@ function App() {
   const [reviews, setReviews] = useState([]);
 
   const getMovies = async () =>{
+    
     try
     {
+
       const response = await api.get("/api/v1/movies");
+
       setMovies(response.data);
+
     } 
     catch(err)
     {
@@ -28,37 +32,44 @@ function App() {
   }
 
   const getMovieData = async (movieId) => {
+     
     try 
     {
-      const response = await api.get(`/api/v1/movies/${movieId}`);
-      const singleMovie = response.data;
-      setMovie(singleMovie);
-      setReviews(singleMovie.reviews);
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
     } 
     catch (error) 
     {
       console.error(error);
     }
+
   }
 
   useEffect(() => {
-  getMovies();
-}, [])
+    getMovies();
+  },[])
 
-return (
-  <div className="App">
-    <Header/>
-    <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home movies={movies} />} ></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-          <Route path="*" element = {<NotFound/>}></Route>
-        </Route>
-    </Routes>
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
+      </Routes>
 
-  </div>
-);
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from 'react-router-dom';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
-  return (
+ 
+return (
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
             <Navbar.Brand href="/" style={{"color":'gold'}}>
-                <FontAwesomeIcon icon ={faVideoSlash}/>Movies
+                <FontAwesomeIcon icon ={faVideoSlash}/>Gold
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
